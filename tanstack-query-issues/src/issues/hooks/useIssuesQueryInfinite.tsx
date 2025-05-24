72d0504a1a99eb7181c77bcde410b4f811da7655
Jesus Cochero <jesus.cochero@onecontact.com.co>
2025-05-23 20:19:21 -0500

    Sección 07 finalizada

--- tanstack-query-issues/src/issues/hooks/useIssuesQueryInfinite.tsx
+++ tanstack-query-issues/src/issues/hooks/useIssuesQueryInfinite.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { getIssues } from '../actions';
 
 interface Props {
@@ -8,10 +8,28 @@ interface Props {
 
 export const useIssuesQueryInfinite = ({ state, selectedLabels }: Props) => {
 
-    const issuesQuery = useQuery({
-        queryKey: ['issues', { state, selectedLabels }],
-        queryFn: () => getIssues(state, selectedLabels, 1),
+    /// el useInfiniteQuery es un hook que permite paginar los resultados de una consulta
+    /// y obtener más resultados a medida que el usuario se desplaza hacia abajo en la lista. eso es lo ideal pero en este ejerccio se hace con el boton obtener mas
+    const issuesQuery = useInfiniteQuery({
+        queryKey: ['issues', 'infinite', { state, selectedLabels }],
+        // el queryFn se usa de esta forma como un objeto para poder usar el pageParam y el queryKey
+        // y poder paginar los resultados. El pageParam es el número de página que se va a obtener.
+        queryFn: ({ pageParam, queryKey}) => {
+            console.log('pageParam', queryKey, pageParam);
+
+
+            /// desestructurar el tercer elemento
+            const [,,args] = queryKey;
+
+            //- los tomamos de aca en caso de no tener acceso desde lo parámetros de la función, pero el mejor tomar los que se reciben como argumento, es lo mismo
+            const { state, selectedLabels } = args as Props;
+
+            /// ya estamos haciendo la paginación infinita
+            return getIssues(state, selectedLabels, pageParam );
+        },
         staleTime: 1000 * 60 * 60,
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, pages) => lastPage.length === 0 ? undefined : pages.length + 1,
     });
 
     
--- tanstack-query-issues/src/issues/views/ListViewInfinite.tsx
+++ tanstack-query-issues/src/issues/views/ListViewInfinite.tsx
@@ -1,19 +1,21 @@
 import LoadingSpinner from '../../shared/components/LoadingSpinner';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
-import { useIssuesQuery } from '../hooks/useIssuesQuery';
 import { useState } from 'react';
+import { useIssuesQueryInfinite } from '../hooks/useIssuesQueryInfinite';
 
 export const ListViewInfinite = () => {
     const [state, setState] = useState<string>('all');
     const [labels, setLabels] = useState<string[]>([]);
 
-    const { issuesQuery } = useIssuesQuery({
+    /// Usar aca el infinity
+    const { issuesQuery } = useIssuesQueryInfinite({
         state,
         selectedLabels: labels,
     });
 
-    const issues = issuesQuery.data || [];
+    /// el useInfiniteQuery es un hook que permite paginar los resultados de una consulta
+    const issues = issuesQuery.data?.pages.flat() || [];
 
     const onLabelsSelected = (label: string) => {
         if (labels.includes(label)) {
@@ -35,12 +37,21 @@ export const ListViewInfinite = () => {
                             onStateChange={(state) => setState(state)}
                             state={state}
                         />
-                        {/*/// cambios aca  */}
+                        {/*/// cambios aca, solo queda un botón para simular desplazamiento  */}
                             <button
                                 className='p-2 bg-blue-500 rounded-md hover:bg-blue-700 transition-all'
-                                onClick={() => console.log('Cargar más')}
+                                /// ya solo con esto obtenemos la demás data
+                                onClick={() => issuesQuery.fetchNextPage()}
+                                disabled={!issuesQuery.hasNextPage || issuesQuery.isFetchingNextPage}
                             >
-                                Cargar más ...
+                                {/* //- podemos poner un loandin por aca */}
+                                {
+                                    issuesQuery.isFetchingNextPage ? (
+                                        'Cargando...'
+                                    ) : (
+                                        'Obtener más'
+                                    )
+                                }
                             </button>
                     </div>
                 )}
